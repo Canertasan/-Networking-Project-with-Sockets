c7063e48c09843d9a19e3851a85b1b93bd1fc7fd
Caner TAŞAN <canertasan@sabanciuniv.edu>
2018-12-12 16:18:46 +0300

    This is last version of Client Side
    This is easier then client part with multithreading. Because we use just 1 clientsocket in that code. We handle some exiting part over there.
    Again it is not a best solution. This is just for educational purpose of socket and multithreading.

--- client/client_CS408/Form1.cs
+++ client/client_CS408/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,6 +21,8 @@ namespace client_CS408
         static String gameCond = "";
         static String turn = "";
         static bool isTurn = false;
+        static bool sameName = true;
+        static bool IsGameContinue = false;
         int portNum = 0;
         Byte[] buffer = new Byte[64];
 
@@ -53,6 +55,13 @@ namespace client_CS408
 
         }
 
+        void SendByte(String sendString)
+        {
+            clientSoc.Send(Encoding.Default.GetBytes(sendString.Length.ToString())); // send name to the server for request.
+            Thread.Sleep(1000);
+            clientSoc.Send(Encoding.Default.GetBytes(sendString)); // send name to the server for request.
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             serverIP = textBox1.Text;
@@ -67,38 +76,40 @@ namespace client_CS408
                 portNum = int.Parse(textBox2.Text); // our port num
                 try
                 {
-                    clientSoc.Connect(serverIP, portNum); //  create connection for request to the server.
-                    clientSoc.Send(Encoding.Default.GetBytes(textBox5.Text)); // send name to the server for request.
-                    if (ReceiveName().Contains("reject")) // reject or not.
+                    while (sameName)
                     {
-                        this.label6.Visible = true;
-                        this.label6.Text = "There is a same name user. Chose different one."; // if is not still connected or some problem with connection.
-                        connected = false;
-                        clientSoc.Close();
-                    }
-                    else
-                    {
-                        connected = true; // if there is no name duplicate then connected. 
-                        this.label6.Visible = false; // these visibilities for some cool GUI :D 
-                        this.textBox3.Visible = false;
-                        this.textBox4.Visible = false;
-                        this.label3.Visible = true;
-                        this.label4.Visible = true;
-                        this.button2.Visible = true;
-                        this.exitButton.Visible = true;
-                        this.richTextBox1.Visible = true;
-                        this.textBox5.Visible = false;
-                        this.textBox2.Visible = false;
-                        this.textBox1.Visible = false;
-                        this.button1.Visible = false;
-                        this.label5.Visible = false;
-                        this.label2.Visible = false;
-                        this.label1.Visible = false;
-                        this.richTextBox1.AppendText("Welcome to Our Question - Answer Game" + Environment.NewLine);
-                        this.Text = textBox5.Text;
-
-                        Thread receiveThread = new Thread(Receive); // start receive thread for taking inputs from server.
-                        receiveThread.Start();
+                        clientSoc.Connect(serverIP, portNum); //  create connection for request to the server.
+                        SendByte(textBox5.Text);
+                        if (ReceiveName().Contains("reject")) // reject or not.
+                        {
+                            this.label6.Visible = true;
+                            this.label6.Text = "There is a same name user. Chose different one."; // if is not still connected or some problem with connection.
+                            connected = false;
+                            clientSoc.Close();
+                        }
+                        else
+                        {
+                            connected = true; // if there is no name duplicate then connected. 
+                            this.label6.Visible = false; // these visibilities for some cool GUI :D 
+                            this.textBox3.Visible = false;
+                            this.textBox4.Visible = false;
+                            this.label3.Visible = true;
+                            this.label4.Visible = true;
+                            this.exitButton.Visible = true;
+                            this.richTextBox1.Visible = true;
+                            this.textBox5.Visible = false;
+                            this.textBox2.Visible = false;
+                            this.textBox1.Visible = false;
+                            this.button1.Visible = false;
+                            this.label5.Visible = false;
+                            this.label2.Visible = false;
+                            this.label1.Visible = false;
+                            this.richTextBox1.AppendText("Welcome to Our Question - Answer Game" + Environment.NewLine);
+                            this.Text = textBox5.Text;
+                            sameName = false;
+                            Thread receiveThread = new Thread(Receive); // start receive thread for taking inputs from server.
+                            receiveThread.Start();
+                        }
                     }
                 }
                 catch
@@ -112,13 +123,13 @@ namespace client_CS408
 
         void Receive()
         {
-            while (connected && !(clientSoc.Poll(1, SelectMode.SelectRead) && clientSoc.Available == 0))
+            while (connected && !(clientSoc.Poll(1, SelectMode.SelectRead) && clientSoc.Available == 0) && !IsGameContinue)
             {
                 try
                 {
                     if (gameCond == "")
                     {
-                        SetRichText("Ready waiting for opponent." + Environment.NewLine);
+                        SetRichText("Ready for waiting game started by server." + Environment.NewLine);
 
                         gameCond = ReceiveName();
 
@@ -129,18 +140,35 @@ namespace client_CS408
                         turn = ReceiveName();
                         if (turn.Contains("Your Turn create a question"))
                         {
+                            SetVisibility(button2, true);
                             SetVisibility(textBox3, true);//visibility changes...
                             SetVisibility(textBox4, true);//visibility changes...
                             isTurn = true;
                             SetRichText("Server: " + turn + Environment.NewLine); // when server send message.
                         }
-                        else
+                        else if (turn.Contains("Wait for question"))
                         {
+                            SetVisibility(button2, false);
+                            SetRichText("Wait for the player to make question" + Environment.NewLine);
                             isTurn = false;
                             SetVisibility(textBox3, true);//visibility changes...
                             String question = ReceiveName();
+                            if (question == "You are the winner and game is finished") // if game finish break the loop
+                            {
+                                SetRichText(question);
+                                break;
+                            }
                             SetRichText("Server: your question: " + question + Environment.NewLine); // when server send message.
-
+                            SetVisibility(button2, true);
+                        }
+                        else// if game finish break the loop
+                        {
+                            if (connected == false)
+                                break;
+                            SetRichText(turn);
+                            Thread.Sleep(5000);
+                            clientSoc.Close();
+                            break;
                         }
                     }
                 }
@@ -153,9 +181,9 @@ namespace client_CS408
             }
             if (clientSoc.Poll(1, SelectMode.SelectRead) && clientSoc.Available == 0)
             {
-                SetRichText("Game is over!" + Environment.NewLine); // if is not still connected or some problem with connection.
+                SetRichText("Your opponent is left! " + Environment.NewLine); // if is not still connected or some problem with connection.
                 clientSoc.Close();
-                Thread.Sleep(5000);
+                Thread.Sleep(10000);
                 System.Windows.Forms.Application.Exit();
             }
         }
@@ -164,21 +192,18 @@ namespace client_CS408
         {
             try
             {
-                clientSoc.Receive(buffer);
-                string name = Encoding.ASCII.GetString(buffer);
                 Array.Clear(buffer, 0, buffer.Length);
-                int index = name.IndexOf("\0");
-                name = name.Substring(0, index);
+                clientSoc.Receive(buffer);
+                int len = int.Parse(Encoding.ASCII.GetString(buffer));
+                Byte[] bufferLen = new Byte[len];
+                clientSoc.Receive(bufferLen);
+                string name = Encoding.ASCII.GetString(bufferLen);
                 return name;
             }
             catch
             {
-                SetRichText("Server is down! Sorry for that :(");
-                Thread.Sleep(5000);
-                clientSoc.Close();
-                System.Windows.Forms.Application.Exit();
+                return "fail";
             }
-            return "exception";
         }
 
 
@@ -210,6 +235,16 @@ namespace client_CS408
 
         }
 
+        public static void SetVisibility(Button t, bool v)// changes the visibility of the given 
+        {
+            t.Invoke((MethodInvoker)delegate
+            {
+                // Running on the UI thread
+                t.Visible = v;
+            });
+
+        }
+
         public static void textMakeEmpty(TextBox t)// changes the visibility of the given 
         {
             t.Invoke((MethodInvoker)delegate
@@ -225,22 +260,25 @@ namespace client_CS408
         {
             if (isTurn && this.textBox4.Text != "" && this.textBox3.Text != "")
             {
-                clientSoc.Send(Encoding.Default.GetBytes(this.textBox4.Text)); // send question.
-                clientSoc.Send(Encoding.Default.GetBytes(this.textBox3.Text));//send answer
+                SendByte(this.textBox4.Text);
+                Thread.Sleep(1000);
+                SendByte(this.textBox3.Text);
                 SetVisibility(textBox4, false); // question.
                 SetVisibility(textBox3, false);
                 textMakeEmpty(textBox4);
                 textMakeEmpty(textBox3);
                 this.label6.Visible = false;
                 SetRichText("Server: Your question and answer received to your opponent." + Environment.NewLine);
+                this.button2.Visible = false;
             }
             else if (!isTurn && this.textBox3.Text != "")
             {
-                clientSoc.Send(Encoding.Default.GetBytes(this.textBox3.Text));//send answer
+                SendByte(this.textBox3.Text);
                 SetVisibility(textBox3, false);
                 textMakeEmpty(textBox3);
                 this.label6.Visible = false;
                 SetRichText("Server: Your answer received to your opponent" + Environment.NewLine);
+                this.button2.Visible = false;
             }
             else
             {
@@ -248,10 +286,28 @@ namespace client_CS408
             }
         }
 
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (connected != false)
+            {
+                base.OnFormClosing(e);
+                SendByte("5");
+                SendByte("Exited");
+                clientSoc.Shutdown(SocketShutdown.Both);
+                clientSoc.Close();
+            }
+        }
+
+
         private void exitButton_Click(object sender, EventArgs e)
         {
+            SendByte("Exited");
+            clientSoc.Shutdown(SocketShutdown.Both);
             clientSoc.Close();
-            Close();
+            SetRichText("EXITING GAME...");
+            Thread.Sleep(400);
+            connected = false;
+            System.Windows.Forms.Application.Exit();
         }
     }
 }
